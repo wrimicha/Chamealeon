8449271008d980b3c2637acaa4a325fb38db55db
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2021-12-05 15:19:58 -0500

    adds empty methods for mealcontroller

--- ChamealeonApp/Controllers/MealController.cs
+++ ChamealeonApp/Controllers/MealController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ChamealeonApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -14,11 +15,38 @@ namespace ChamealeonApp.Controllers
     {
         //Amir
         //POST add new user meal to db
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User modelUser, Meal modelMeal)
+        {
+            //TODO: Implement Realistic Implementation
+            await Task.Yield();
+            return Created("", null);
+        }
 
         //PUT possibly
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] User modelUser)
+        {
+            //TODO: Implement Realistic Implementation
+            await Task.Yield();
+            return Ok();
+        }
 
         //DELETE delete user meal?
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            //TODO: Implement Realistic Implementation
+            return Ok();
+        }
 
         //GET full details of a meal (screen, has instructions etc)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get()
+        {
+            //TODO: Implement Realistic Implementation
+            await Task.Yield();
+            return Ok();
+        }
     }
 }
