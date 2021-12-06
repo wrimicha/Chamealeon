1e47f25bfb8cc30d72b0bad83447126c3441dece
Burhan Faquiri <59700142+burhanf@users.noreply.github.com>
2021-12-05 20:47:34 -0500

    Creates initial implementation of controllers

---
+++ ChamealeonApp/Models/DTOs/MealPlanQueryDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChamealeonApp.Models.DTOs
+{
+    public class MealPlanQueryDTO
+    {
+        //properties of meal plan query
+    }
+}
