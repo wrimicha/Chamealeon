876cdd8b5190a8e5ff604c4dfe950773ede71d0e
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2021-12-05 20:20:31 -0500

    adds ctor to mealplan controller

--- ChamealeonApp/Controllers/MealController.cs
+++ ChamealeonApp/Controllers/MealController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using ChamealeonApp.Models.Entities;
+using ChamealeonApp.Models.Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -14,38 +15,43 @@ namespace ChamealeonApp.Controllers
 
     public class MealController : Controller
     {
+        private readonly DataContext _context;
+        public MealController(DataContext context)
+        {
+            _context = context;
+        }
         //Amir
         //POST add new user meal to db
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] User modelUser, Meal modelMeal)
+        [HttpPost("{id}")]
+        public async Task<IActionResult> AddCustomMealAsync(string id, [FromBody] Meal modelMeal)
         {
-            //TODO: Implement Realistic Implementation
+            // TODO depends on ef core implementation for now
             await Task.Yield();
             return Created("", null);
         }
 
         //PUT possibly
-        [HttpPut]
-        public async Task<IActionResult> Put([FromBody] User modelUser)
+        // [HttpPut]
+        // public async Task<IActionResult> UpdateMealAsync([FromBody] User modelUser)
+        // {
+
+        //     await Task.Yield();
+        //     return Ok();
+        // }
+
+        //DELETE delete user meal?
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string id)
         {
             //TODO: Implement Realistic Implementation
             await Task.Yield();
             return Ok();
         }
-
-        //DELETE delete user meal?
-        [HttpDelete]
-        public IActionResult Delete(string id)
-        {
-            //TODO: Implement Realistic Implementation
-            return Ok();
-        }
-
         //GET full details of a meal (screen, has instructions etc)
         [HttpGet("{id}")]
         public async Task<IActionResult> Get()
         {
-            //TODO: Implement Realistic Implementation
+
             await Task.Yield();
             return Ok();
         }
