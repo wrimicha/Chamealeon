aff0c03b95746142ad6a78f56d1e88ef73ea66cb
Burhan Faquiri <59700142+burhanf@users.noreply.github.com>
2021-12-13 21:42:49 -0500

    Update migrations and db

--- ChamealeonApp/Controllers/MealPlanController.cs
+++ ChamealeonApp/Controllers/MealPlanController.cs
@@ -42,29 +42,29 @@ namespace ChamealeonApp.Controllers
         [HttpPost("generateMealPlan")]
         public async Task<IActionResult> GenereateMealPlan([FromBody] MealPlanQueryDTO mealPlanQuery)
         {
-            try
-            {
-                //get the logged in user 
-                var loggedInUser = await _userManager.Users.Include(u => u.CurrentMealPlan).Include(u => u.PersonalNutritionalInformationGoal).FirstOrDefaultAsync(us => us.NormalizedEmail
-                .Equals(User.FindFirstValue(ClaimTypes.Email).ToUpper()));
+            // try
+            // {
+            //get the logged in user 
+            var loggedInUser = await _userManager.Users.Include(u => u.CurrentMealPlan).Include(u => u.PersonalNutritionalInformationGoal).FirstOrDefaultAsync(us => us.NormalizedEmail
+            .Equals(User.FindFirstValue(ClaimTypes.Email).ToUpper()));
 
-                //call helper to make a request to API
-                var retrievedRootResponse = await SpoonacularAPIHelper.GenerateMealPlanFromSpoonacularAsync(loggedInUser.Diet.Trim(), mealPlanQuery.ItemsToExclude, loggedInUser.PersonalNutritionalInformationGoal.Calories);
+            //call helper to make a request to API
+            var retrievedRootResponse = await SpoonacularAPIHelper.GenerateMealPlanFromSpoonacularAsync(loggedInUser.Diet.Trim(), mealPlanQuery.ItemsToExclude, loggedInUser.PersonalNutritionalInformationGoal.Calories);
 
-                //take the API response and convert to a proper meal plan
-                var convertedMealPlan = await MealPlanResponseHelper.ConvertRootDTOToMealPlanAsync(retrievedRootResponse, _context);
+            //take the API response and convert to a proper meal plan
+            var convertedMealPlan = await MealPlanResponseHelper.ConvertRootDTOToMealPlanAsync(retrievedRootResponse, _context);
 
-                //add the meal plan to the db and to the user
-                loggedInUser.CurrentMealPlan = convertedMealPlan;
-                await _context.SaveChangesAsync();
+            //add the meal plan to the db and to the user
+            loggedInUser.CurrentMealPlan = convertedMealPlan;
+            await _context.SaveChangesAsync();
 
-                return Ok(convertedMealPlan);
-            }
-            catch (System.Exception)
-            {
+            return Ok(convertedMealPlan);
+            //}
+            // catch (System.Exception)
+            // {
 
-                return BadRequest(new ErrorDTO { Title = "An error has occured generating the meal plan." });
-            }
+            //     return BadRequest(new ErrorDTO { Title = "An error has occured generating the meal plan." });
+            // }
 
         }
 
@@ -72,7 +72,7 @@ namespace ChamealeonApp.Controllers
         //Author: Burhan
         //Update a specific meal in the weekly meal plan with a meal made by the user
 
-        
+
         [Authorize]
         [HttpPut("updateMealPlanWithUserMeal")]
 
@@ -80,35 +80,35 @@ namespace ChamealeonApp.Controllers
         //each day has a list of 3 meals, indices are 0,1 and 2
         public async Task<IActionResult> UpdateMealPlanWithUserMeal(string mealId, DayOfWeek day = DayOfWeek.Sunday, int mealIndexInDay = 0)
         {
-            try
+            // try
+            // {
+            if (mealIndexInDay > 2 || mealIndexInDay < 0)
             {
-                if (mealIndexInDay > 2 || mealIndexInDay < 0)
-                {
-                    return BadRequest(new ErrorDTO { Title = "Invalid meal selected to update." });
-                }
-
-                //get the logged in user 
-                var loggedInUser = await _userManager.Users.Include(u => u.CurrentMealPlan).ThenInclude(m => m.MealDays).ThenInclude(md => md.Meals).FirstOrDefaultAsync(us => us.NormalizedEmail
-                .Equals(User.FindFirstValue(ClaimTypes.Email).ToUpper()));
-
-                //find the meal in the database that they want to replace with, the spoonacular id should be empty if its a user defined meal
-                var mealInDb = _context.Meals.Where(m => string.IsNullOrEmpty(m.SpoonacularMealId.ToString()) == true)
-                .FirstOrDefaultAsync(userMeals => userMeals.Id.Equals(new Guid(mealId.Trim())));
-
-                //replace the user meal with the new meal by getting the meal plan, get the day of the week, get the meal object in the list of meals for that day
-                //because each enum value is also the index in the list, we can cast the enum to an int (ex. 0 = Sunday)
-                loggedInUser.CurrentMealPlan.MealDays[(int)day].Meals[mealIndexInDay] = await mealInDb;
-
-                await _context.SaveChangesAsync();
-
-                //show the updated meal for that day
-                return Ok(loggedInUser.CurrentMealPlan.MealDays[(int)day].Meals.ToList());
+                return BadRequest(new ErrorDTO { Title = "Invalid meal selected to update." });
             }
-            catch (System.Exception)
-            {
 
-                return BadRequest(new ErrorDTO { Title = "An error has occured updating the meal plan." });
-            }
+            //get the logged in user 
+            var loggedInUser = await _userManager.Users.Include(u => u.CurrentMealPlan).ThenInclude(m => m.MealDays).ThenInclude(md => md.Meals).FirstOrDefaultAsync(us => us.NormalizedEmail
+            .Equals(User.FindFirstValue(ClaimTypes.Email).ToUpper()));
+
+            //find the meal in the database that they want to replace with, the spoonacular id should be empty if its a user defined meal
+            var mealInDb = _context.Meals.Where(m => string.IsNullOrEmpty(m.SpoonacularMealId.ToString()) == true)
+            .FirstOrDefaultAsync(userMeals => userMeals.Id.Equals(new Guid(mealId.Trim())));
+
+            //replace the user meal with the new meal by getting the meal plan, get the day of the week, get the meal object in the list of meals for that day
+            //because each enum value is also the index in the list, we can cast the enum to an int (ex. 0 = Sunday)
+            loggedInUser.CurrentMealPlan.MealDays[(int)day].Meals[mealIndexInDay] = await mealInDb;
+
+            await _context.SaveChangesAsync();
+
+            //show the updated meal for that day
+            return Ok(loggedInUser.CurrentMealPlan.MealDays[(int)day].Meals.ToList());
+            // }
+            // catch (System.Exception)
+            // {
+
+            //     return BadRequest(new ErrorDTO { Title = "An error has occured updating the meal plan." });
+            // }
         }
 
         // Mike
@@ -129,7 +129,7 @@ namespace ChamealeonApp.Controllers
                                                  .ThenInclude(md => md.Meals)
                                                  .FirstOrDefaultAsync(us => us.NormalizedEmail
                                                  .Equals(User.FindFirstValue(ClaimTypes.Email).ToUpper()));
-  
+
             //Use the helper function to create a meal object from the spoonacular meal id
             var spoonacularMeal = MealPlanResponseHelper.ConvertSpoonacularMealToFullMealAsync(spoonacularId, _context);
 
@@ -153,7 +153,7 @@ namespace ChamealeonApp.Controllers
             //get the user meals order by the day of the week
             var loggedInUser = await _userManager.Users
                                                  .Include(u => u.CurrentMealPlan)
-                                                 .ThenInclude(m => m.MealDays.OrderBy(md=>md.Day)) //.OrderBy(md=>md.Day) - Don't need this days already in order
+                                                 .ThenInclude(m => m.MealDays.OrderBy(md => md.Day)) //.OrderBy(md=>md.Day) - Don't need this days already in order
                                                  .ThenInclude(md => md.Meals)
                                                  .ThenInclude(i => i.Ingredients)
                                                  .FirstOrDefaultAsync(us => us.NormalizedEmail
