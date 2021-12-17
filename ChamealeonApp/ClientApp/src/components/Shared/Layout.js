36598e1408efdf83fd1b6a4567b1492737454173
Amir Azizafshari <a_amirhossein@icloud.com>
2021-12-17 12:16:45 -0500

    fixed todo comments

--- ChamealeonApp/ClientApp/src/components/Shared/Layout.js
+++ ChamealeonApp/ClientApp/src/components/Shared/Layout.js
@@ -3,7 +3,7 @@ import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
 export const Layout = ({ authHook, children, ...rest }) => {
-
+// TODO fix navmenu
   return (
     <div>
       <NavMenu authHook={authHook}/>
